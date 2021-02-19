4687dbb9dc6801abfed134eb9237a1267e9d65b2
Wakahuko <73422734+Wakahuko@users.noreply.github.com>
2021-02-19 16:24:12 +0100

    Add files via upload

---
+++ DietaPudelkowa/Models/CalPriceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietaPudelkowa.Models
+{
+    public class CalPriceModel
+    {
+        public int Cal { get; set; }
+        public int Price { get; set; }
+
+        public string Names { get; set; }
+    }
+}
---
+++ DietaPudelkowa/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+
+namespace DietaPudelkowa.Models
+{
+    public class Product
+    {
+        public string Nazwa { get; set; }
+        public string Rodzaj { get; set; }
+        public int Cal { get; set; }
+        public int Cena { get; set; }
+        public bool Wege { get; set; }
+    }
+}
