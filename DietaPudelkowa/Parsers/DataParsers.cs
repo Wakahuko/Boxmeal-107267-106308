26b538ffaafb4ca6e6fa84698cb542730a06b5ce
Wakahuko <73422734+Wakahuko@users.noreply.github.com>
2021-02-20 13:26:45 +0100

    Add files via upload

---
+++ DietaPudelkowa/Parsers/DataParsers.cs
@@ -0,0 +1,138 @@
+﻿using CsvHelper;
+using DietaPudelkowa.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DietaPudelkowa.Parsers
+{
+    public class DataParsers
+    {
+        private string _filePath = ConfigurationManager.AppSettings["filePath"]??"";
+        private IEnumerable<Product> _productList;
+        private readonly Random _random = new Random();
+
+       public DataParsers(string path = "")
+        {
+            if (!String.IsNullOrEmpty(path))
+                _filePath = path;
+
+            if (!File.Exists(_filePath))
+            {
+                Console.WriteLine("Plik nie istnieje.");
+
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            else
+            {
+                using (var reader = new StreamReader(_filePath))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    _productList = csv.GetRecords<Product>().ToList();
+                }
+            }
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _productList;
+        }
+
+        public void ShowAllProducts()
+        {
+            foreach(var item in _productList)
+            {
+                string wege = item.Wege ? "TAK" : "NIE";
+                Console.WriteLine($"{item.Nazwa} ({item.Rodzaj}),{item.Cal} Cal,{item.Cena} PLN, WEGETARAŃSKIE: {wege}");
+            }
+        }
+
+        public void GenerateDiet(float cal, bool wege)
+        {
+            List<DayOfWeek> workDays = new List<DayOfWeek>();
+            workDays.Add(DayOfWeek.Monday);
+            workDays.Add(DayOfWeek.Tuesday);
+            workDays.Add(DayOfWeek.Wednesday);
+            workDays.Add(DayOfWeek.Thursday);
+            workDays.Add(DayOfWeek.Friday);
+
+
+            foreach (var day in workDays)
+            {
+                Console.Write(day);
+
+                Console.Write("\nŚniadanie (");
+                var sniadanie = GenerateThreeProducts(cal, wege);
+                Console.Write($"{sniadanie.Names}) {sniadanie.Cal} cal\n");
+
+                Console.Write("Obiad (");
+                var obiad = GenerateThreeProducts(cal, wege);
+                Console.Write($"{obiad.Names}) {obiad.Cal} cal\n");
+
+                Console.Write("Kolacja (");
+                var kolacja = GenerateThreeProducts(cal, wege);
+                Console.Write($"{kolacja.Names}) {kolacja.Cal} cal\n");
+                Console.WriteLine($"Wartość Kaloryczna Ogółem: {sniadanie.Cal + obiad.Cal + kolacja.Cal}\n");
+
+            }
+        }
+
+        public CalPriceModel GenerateThreeProducts(float cal, bool ifWege)
+        {
+            string result="";
+            int sumCal = 0;
+            int sumPrice = 0;
+            List<Product> products = new List<Product>();
+
+            if (ifWege == true)
+                products = _productList.Where(p => p.Wege == ifWege).ToList();
+            else
+                products = _productList.ToList();
+
+            while (true)
+            {
+                int index;
+
+                index = new Random().Next(0,products.Where(p => p.Rodzaj == "Przystawka").Count());
+                var productPrzystawka = products.Where(p => p.Rodzaj == "Przystawka").ElementAt(index);
+                result += productPrzystawka.Nazwa+"/";
+                sumCal += productPrzystawka.Cal;
+                sumPrice += productPrzystawka.Cena;
+                //Thread.Sleep(10);
+
+                index = _random.Next(products.Where(p => p.Rodzaj == "Główne").Count());
+                var productGlowne = products.Where(p => p.Rodzaj == "Główne").ElementAt(index);
+                result += productGlowne.Nazwa+"/";
+                sumCal += productGlowne.Cal;
+                sumPrice += productGlowne.Cena;
+                //Thread.Sleep(10);
+
+                index = _random.Next(products.Where(p => p.Rodzaj == "Deser").Count());
+                var productDeser = products.Where(p => p.Rodzaj == "Deser").ElementAt(index);
+                result += productDeser.Nazwa;
+                sumCal += productDeser.Cal;
+                sumPrice += productDeser.Cena;
+                //Thread.Sleep(10);
+
+                if (sumCal <= (cal + cal * 0.1) && sumCal >= (cal - cal * 0.1))
+                    break;
+                else
+                {
+                    sumCal = 0;
+                    result = "";
+                }
+            }
+
+            return new CalPriceModel() { Cal = sumCal, Price = sumPrice, Names = result };
+        }
+
+
+    }
+}
---
+++ DietaPudelkowa/Program.cs
@@ -0,0 +1,91 @@
+﻿using DietaPudelkowa.Models;
+using DietaPudelkowa.Parsers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietaPudelkowa
+{
+    class Program
+    {
+        static void GetProducts(IEnumerable<Product> items)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            DataParsers _parser = new DataParsers();
+            var items = _parser.GetAllProducts();
+            int sumCam = 0;
+            bool wege = true;
+
+            Console.WriteLine("DIETA PUDEŁKOWA\n");
+            Console.WriteLine("Co chcesz zrobić (wpisz a lub b): ");
+            Console.Write("a - zamówić dietę\nb - wyświetlić dostępne składniki\n");
+
+            char info = Console.ReadKey().KeyChar;
+
+            while(info!='b' && info != 'a')
+            {
+                Console.WriteLine("\n\nPodałeś błędne dane.\nCo chcesz zrobić (wpisz a lub b): ");
+                Console.Write("a - zamówić dietę\nb - wyświetlić dostępne składniki\n");
+
+                info = Console.ReadKey().KeyChar;
+            }
+
+            if(info == 'b')
+            {
+                Console.WriteLine("\nSkładniki:\n\n");
+                _parser.ShowAllProducts();
+            }
+
+            if(info=='a')
+            {
+                Console.WriteLine("\n\nProszę podać wartość kaloryczną diety:\na. 1000 cal,\nb. 1500 cal,\nc. 2000 cal,\nd. 2500 cal,\ne. 3000 cal\n");
+
+                info = Console.ReadKey().KeyChar;
+
+                switch(info)
+                {
+                    case 'a':
+                        sumCam = 1000;
+                        break;
+                    case 'b':
+                        sumCam = 1500;
+                        break;
+                    case 'c':
+                        sumCam = 2000;
+                        break;
+                    case 'd':
+                        sumCam = 2500;
+                        break;
+                    case 'e':
+                        sumCam = 3000;
+                        break;
+                    default:
+                        Console.WriteLine("\nPodano błędny znak.\n");
+                        break;
+                }
+
+                Console.WriteLine("\nProszę podać czy dieta ma być wegetariańska: tak(T), nie(N)\n");
+
+                info = Console.ReadKey().KeyChar;
+
+                if (info == 't')
+                    wege = true;
+               else if (info == 'n')
+                    wege = false;
+
+                _parser.GenerateDiet(sumCam / 3, wege);
+            }
+
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
